1ff24c23f1f1f8a59c56fc94c7d48796a20428b7
Divyanshu-Sharma-2004 <divyanshusharma7982@gmail.com>
2024-08-31 23:21:22 +0530

    Removed unneccesary commented code

--- RandaomPasswordGenrator.py
+++ RandaomPasswordGenrator.py
@@ -1,19 +1,3 @@
-'''
-import random
-import string
-
-charValues = string.ascii_letters + string.digits + string.punctuation
-pass_len = int(input("Enter your desired password length : "))
-
-
-password = ""
-
-for i in range(pass_len):
-    password += random.choice(charValues)
-
-print("your random password is : ",password)
-'''
-
 # List comprehension technique
 
 import random
